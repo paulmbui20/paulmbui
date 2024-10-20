cf24cbc06f16bf4ecf64ca951a75e576bf278868
MP <paulmbui20@gmail.com>
2024-10-20 19:27:32 +0300

    Revert "contact form functionality added"
    This reverts commit f68636f8eaebd42a461cffe6f826ade778cda735.

--- js/app.js
+++ js/app.js
@@ -5,37 +5,3 @@ const mobileMenu = document.getElementById('mobile-menu');
     menuItems.classList.toggle('active');
   });
 
-  function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Smooth scrolling effect
-    });
-}
-$(document).ready(function() {
-  $('#contact-form').on('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    // Get the form data
-    var formData = {
-      name: $('#name').val(),
-      email: $('#email').val(),
-      phone: $('#phone').val(),
-      message: $('#message').val()
-    };
-
-    // Send the form data to Google Apps Script
-    $.ajax({
-      url: 'https://script.google.com/macros/s/AKfycbz4T0VxjYq17YUs7iHQfuP2xBOJXJAAYjMOdqzICbI6KEMmPUWDcwPAL-vwqt2B8Hvo/exec', // Replace with your Web App URL
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify(formData),
-      success: function(response) {
-        $('#response').html('<p>Thank you! Your message has been sent.</p>');
-        $('#contact-form')[0].reset(); // Reset the form after submission
-      },
-      error: function() {
-        $('#response').html('<p>Oops! There was an error. Please try again.</p>');
-      }
-    });
-  });
-});
